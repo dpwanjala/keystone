aeafd0ee1eb257dd11e05ce3c19c42225ec06b46
dpwanjala <dpwanjala@gmail.com>
2022-02-17 18:16:23 -0800

    remove student-preference-rating

--- keystone-cms-app-strapi/src/api/student-preference-rating/routes/student-preference-rating.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * student-preference-rating router.
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::student-preference-rating.student-preference-rating');
